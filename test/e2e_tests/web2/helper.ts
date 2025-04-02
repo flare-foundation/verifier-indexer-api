d0b8be5344e7c0b696c06d535a37b1bfff30d750
Urška Krivc <urska.krivc@abelium.com>
2025-04-02 10:37:57 +0200

    add tests

--- test/e2e_tests/web2/helper.ts
+++ test/e2e_tests/web2/helper.ts
@@ -19,9 +19,9 @@ import { LoggerMiddleware } from '../../../src/middleware/LoggerMiddleware';
 import { IJsonApiVerifierService } from '../../../src/services/ijson-api-verifier.service';
 import { IJsonApiConfig } from 'src/config/interfaces/json-api';
 import { VerifierServerConfig, IConfig } from 'src/config/interfaces/common';
+import { HTTP_METHOD } from '../../../src/verification/json-api/utils';
 
 function getConfig() {
-  const api_keys = process.env.API_KEYS?.split(',') || [''];
   const verifier_type = ChainType.WEB2;
   const isTestnet = process.env.TESTNET == 'true';
 
@@ -35,14 +35,14 @@ function getConfig() {
 
   const apiJsonDefaultConfig: IJsonApiConfig = {
     securityConfig: {
-      blockHostnames: [],
+      blockHostnames: ["www.google.com"],
       blockJq: [],
       blockJson: [],
       jqVersion: '1.7.1',
     },
     sourceConfig: {
       requiresApiKey: false,
-      allowedMethods: '*',
+      allowedMethods: [HTTP_METHOD.GET],
       allowedEndPoints: '*',
       maxResponseSize: 1024 * 1024,
       maxTimeout: 1000,
@@ -92,3 +92,118 @@ before(async () => {
 after(async () => {
   await app.close();
 });
+
+
+// constants used in test
+const api_keys = process.env.API_KEYS?.split(',') || [''];
+export const api_key = api_keys[0];
+export const payload = {
+  attestationType: "0x494a736f6e417069000000000000000000000000000000000000000000000000",
+  sourceId: "0x7465737457454232000000000000000000000000000000000000000000000000",
+  requestBody: {
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    http_method: "GET",
+    headers: "{\"Content-Type\":\"application/json\"}",
+    query_params: "{}",
+    body: "{}",
+    postprocess_jq: ".title",
+    abi_signature: "{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"}"
+  }
+};
+export const attResponse = {
+  attestationType: "0x494a736f6e417069000000000000000000000000000000000000000000000000",
+  sourceId: "0x7465737457454232000000000000000000000000000000000000000000000000",
+  votingRound: "0",
+  lowestUsedTimestamp: "0",
+  requestBody: {
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    http_method: "GET",
+    headers: "{\"Content-Type\":\"application/json\"}",
+    query_params: "{}",
+    body: "{}",
+    postprocess_jq: ".title",
+    abi_signature: "{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"}"
+  },
+  responseBody: {
+    abi_encoded_data:
+      '0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001264656c65637475732061757420617574656d0000000000000000000000000000',
+  },
+};
+export const abiEncoding: unknown = {
+  components: [
+    {
+      internalType: 'bytes32',
+      name: 'attestationType',
+      type: 'bytes32',
+    },
+    { internalType: 'bytes32', name: 'sourceId', type: 'bytes32' },
+    { internalType: 'uint64', name: 'votingRound', type: 'uint64' },
+    {
+      internalType: 'uint64',
+      name: 'lowestUsedTimestamp',
+      type: 'uint64',
+    },
+    {
+      components: [
+        {
+          internalType: 'string',
+          name: 'url',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: 'http_method',
+          type: 'string',
+        },
+        {
+          "internalType": "string",
+          "name": "headers",
+          "type": "string"
+        },
+        {
+          internalType: 'string',
+          name: 'query_params',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: 'body',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: 'postprocess_jq',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: 'abi_signature',
+          type: 'string',
+        },
+      ],
+      internalType: 'struct IJsonApi.RequestBody',
+      name: 'requestBody',
+      type: 'tuple',
+    },
+    {
+      components: [
+        {
+          internalType: 'bytes',
+          name: 'abi_encoded_data',
+          type: 'bytes',
+        },
+      ],
+      internalType: 'struct IJsonApi.ResponseBody',
+      name: 'responseBody',
+      type: 'tuple',
+    },
+  ],
+  internalType: 'struct IJsonApi.Response',
+  name: '_response',
+  type: 'tuple',
+};
+export const abiEncodedData =
+  '0x' +
+  '0000000000000000000000000000000000000000000000000000000000000020' +
+  '0000000000000000000000000000000000000000000000000000000000000012' +
+  '64656c65637475732061757420617574656d0000000000000000000000000000';
--- test/e2e_tests/web2/ijson_api.e2e-spec.ts
+++ test/e2e_tests/web2/ijson_api.e2e-spec.ts
@@ -1,161 +1,254 @@
 import { expect } from 'chai';
 import { ethers } from 'ethers';
 import * as request from 'supertest';
-import { app } from './helper';
+import { abiEncodedData, abiEncoding, api_key, app, attResponse, payload } from './helper';
+import { AttestationResponseStatus } from '../../../src/verification/response-status';
 
 describe('/JsonApi/prepareResponse', () => {
   it('should get right responseBody', async () => {
-    const payload = 
-      {
-        attestationType: "0x494a736f6e417069000000000000000000000000000000000000000000000000",
-        sourceId: "0x7465737457454232000000000000000000000000000000000000000000000000",
-        requestBody: {
-          url: "https://jsonplaceholder.typicode.com/todos/1",
-          http_method: "GET",
-          headers: "{\"Content-Type\":\"application/json\"}",
-          query_params: "{\"userId\":1}",
-          body: "{\"userId\":1,\"completed\":false}",
-          postprocess_jq: ".title",
-          abi_signature: "{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"}"
-        }
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(payload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.VALID);
+    expect(responseBody.response.responseBody.abi_encoded_data).to.be.equal(
+      abiEncodedData,
+    );
+  });
+
+  it('Should reject - private IP', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        url: "https://localhost:3000",
+      },
     };
 
     const response = await request(app.getHttpServer())
       .post('/JsonApi/prepareResponse')
-      .send(payload)
-      .set('X-API-KEY', '12345')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
       .expect(200)
       .expect('Content-Type', /json/);
 
-    const abiEncodedData =
-      '0x' +
-      '0000000000000000000000000000000000000000000000000000000000000020' +
-      '0000000000000000000000000000000000000000000000000000000000000012' +
-      '64656c65637475732061757420617574656d0000000000000000000000000000';
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_SOURCE_URL);
+  });
+
+  it('Should reject - private IP 2', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        url: "https://%31%32%37%2e0.0.1",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
 
     const responseBody = response.body;
-    expect(responseBody.status).to.be.equal('VALID');
-    expect(responseBody.response.responseBody.abi_encoded_data).to.be.equal(
-      abiEncodedData,
-    );
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_SOURCE_URL);
+  });
+
+  it('Should reject - blocked hostname', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        url: "https://www.google.com",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_SOURCE_URL);
+  });
+
+  it('Should reject - invalid url', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        url: "http://localhost:3000",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_SOURCE_URL);
+  });
+
+  it('Should reject - fetch error', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        url: "https://stackoverflow.com",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_FETCH_ERROR);
+  });
+
+  it('Should reject - invalid http method', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        http_method: "POST"
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_HTTP_METHOD);
+  });
+
+  it('Should reject - invalid headers', async () => {
+    const malformedPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        headers: "{\"Content-Type\":\"application/json\"",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(malformedPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_HEADERS);
+  });
+
+  it('Should reject - invalid query params', async () => {
+    const malformedPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        query_params: "{",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(malformedPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_QUERY_PARAMS);
+  });
+
+  it('Should reject - invalid body', async () => {
+    const malformedPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        body: "{",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(malformedPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_BODY);
+  });
+
+  it('Should reject - invalid abi signature', async () => {
+    const malformedPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        abi_signature: "{",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(malformedPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_ABI_SIGNATURE);
+  });
+
+  it('Should reject - invalid encode error', async () => {
+    const customPayload = {
+      ...payload,
+      requestBody: {
+        ...payload.requestBody,
+        abi_signature: "{\"components\": [{\"internalType\": \"uint8\",\"name\": \"userId\",\"type\": \"uint8\"},{\"internalType\": \"uint8\",\"name\": \"id\",\"type\": \"uint8\"},{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"},{\"internalType\": \"bool\",\"name\": \"completed\",\"type\": \"bool\"}],\"name\": \"task\",\"type\": \"tuple\"}",
+      },
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/JsonApi/prepareResponse')
+      .send(customPayload)
+      .set('X-API-KEY', api_key)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const responseBody = response.body;
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.INVALID_ENCODE_ERROR);
   });
 });
 
 describe('/JsonApi/mic', () => {
   it('should get right responseBody', async () => {
-    const payload = {
-      attestationType: "0x494a736f6e417069000000000000000000000000000000000000000000000000",
-      sourceId: "0x7465737457454232000000000000000000000000000000000000000000000000",
-      requestBody: {
-        url: "https://jsonplaceholder.typicode.com/todos/1",
-        http_method: "GET",
-        headers: "{\"Content-Type\":\"application/json\"}",
-        query_params: "{\"userId\":1}",
-        body: "{\"userId\":1,\"completed\":false}",
-        postprocess_jq: ".title",
-        abi_signature: "{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"}"
-      }
-    };
     const response = await request(app.getHttpServer())
       .post('/JsonApi/mic')
       .send(payload)
-      .set('X-API-KEY', '12345')
+      .set('X-API-KEY', api_key)
       .expect(200)
       .expect('Content-Type', /json/);
-
-    const attResponse = {
-      attestationType: "0x494a736f6e417069000000000000000000000000000000000000000000000000",
-      sourceId: "0x7465737457454232000000000000000000000000000000000000000000000000",
-      votingRound: "0",
-      lowestUsedTimestamp: "0",
-      requestBody: {
-        url: "https://jsonplaceholder.typicode.com/todos/1",
-        http_method: "GET",
-        headers: "{\"Content-Type\":\"application/json\"}",
-        query_params: "{\"userId\":1}",
-        body: "{\"userId\":1,\"completed\":false}",
-        postprocess_jq: ".title",
-        abi_signature: "{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"}"
-      },
-      responseBody: {
-        abi_encoded_data:
-          '0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001264656c65637475732061757420617574656d0000000000000000000000000000',
-      },
-    };
-
-    const abiEncoding: unknown = {
-      components: [
-        {
-          internalType: 'bytes32',
-          name: 'attestationType',
-          type: 'bytes32',
-        },
-        { internalType: 'bytes32', name: 'sourceId', type: 'bytes32' },
-        { internalType: 'uint64', name: 'votingRound', type: 'uint64' },
-        {
-          internalType: 'uint64',
-          name: 'lowestUsedTimestamp',
-          type: 'uint64',
-        },
-        {
-          components: [
-            {
-              internalType: 'string',
-              name: 'url',
-              type: 'string',
-            },
-            {
-              internalType: 'string',
-              name: 'http_method',
-              type: 'string',
-            },
-            {
-              "internalType": "string",
-              "name": "headers",
-              "type": "string"
-            },
-            {
-              internalType: 'string',
-              name: 'query_params',
-              type: 'string',
-            },
-            {
-              internalType: 'string',
-              name: 'body',
-              type: 'string',
-            },
-            {
-              internalType: 'string',
-              name: 'postprocess_jq',
-              type: 'string',
-            },
-            {
-              internalType: 'string',
-              name: 'abi_signature',
-              type: 'string',
-            },
-          ],
-          internalType: 'struct IJsonApi.RequestBody',
-          name: 'requestBody',
-          type: 'tuple',
-        },
-        {
-          components: [
-            {
-              internalType: 'bytes',
-              name: 'abi_encoded_data',
-              type: 'bytes',
-            },
-          ],
-          internalType: 'struct IJsonApi.ResponseBody',
-          name: 'responseBody',
-          type: 'tuple',
-        },
-      ],
-      internalType: 'struct IJsonApi.Response',
-      name: '_response',
-      type: 'tuple',
-    };
 
     const abiEncoded = ethers.AbiCoder.defaultAbiCoder().encode(
       [abiEncoding as ethers.ParamType, 'string'],
@@ -164,7 +257,7 @@ describe('/JsonApi/mic', () => {
     const mic = ethers.keccak256(abiEncoded);
 
     const responseBody = response.body;
-    expect(responseBody.status).to.be.equal('VALID');
+    expect(responseBody.status).to.be.equal(AttestationResponseStatus.VALID);
     expect(responseBody.messageIntegrityCode).to.be.equal(mic);
   });
 });
