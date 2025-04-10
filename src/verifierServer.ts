c1498598c8202c8775148b636c27afb107489b6a
Urška Krivc <urska.krivc@abelium.com>
2025-04-10 11:19:50 +0200

    limit requests to 100kb

--- src/verifierServer.ts
+++ src/verifierServer.ts
@@ -35,8 +35,8 @@ export async function runVerifierServer() {
   const logger = new Logger();
 
   app.use(helmet());
-  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-  app.use(express.json({ limit: '1mb' }));
+  app.use(express.urlencoded({ extended: true })); // default limit: '100kb'
+  app.use(express.json()); // default limit: '100kb'
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
 
