ba60810e95962a675c6684a664f92e66dbca0f44
Andrius Dagys <andrius@flare.network>
2025-11-19 11:23:41 -0300

    Add Ignite prod source

--- src/config/web2/web2-json-sources.ts
+++ src/config/web2/web2-json-sources.ts
@@ -1,4 +1,20 @@
-import { Web2JsonSource } from '../interfaces/web2-json';
+import { AuthType, HTTP_METHOD, Web2JsonSource } from '../interfaces/web2-json';
 
 /** Allowed Web2Json source definitions for mainnet deployments. */
-export const WEB2_JSON_SOURCES: Web2JsonSource[] = [];
+export const WEB2_JSON_SOURCES: Web2JsonSource[] = [
+  {
+    sourceId: 'Ignite',
+    endpoints: [
+      {
+        host: 'api-proxy.ignitemarket.xyz',
+        paths: '*',
+        methods: [HTTP_METHOD.GET],
+        auth: {
+          type: AuthType.APIKEY,
+          env: 'IGNITE_API_KEY',
+          header: 'x-api-key',
+        },
+      },
+    ],
+  },
+];
